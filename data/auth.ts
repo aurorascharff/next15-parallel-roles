b12aaca7c615e1e80058c69c4e6e4023a58a8f22
Aurora Scharff <aurora.sofie@gmail.com>
2024-11-15 15:45:27 +0100

    update

--- data/auth.ts
+++ data/auth.ts
@@ -1,6 +1,5 @@
 'use server';
 
-import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { cache } from 'react';
 import type { RoleClaim } from '@/types/role';
@@ -12,7 +11,6 @@ export async function setRoleClaim(newRoleClaim: RoleClaim) {
     return setTimeout(resolve, 1000);
   });
   roleClaim = newRoleClaim;
-  revalidatePath('/dashboard'); // TODO: Replace with expirePath once imports don't fail
   redirect('/dashboard');
 }
 
